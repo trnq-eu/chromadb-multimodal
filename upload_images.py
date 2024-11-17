11ed297ca7228ed2b91f66b91362de8dff0e9908
trnq-eu <48558947+trnq-eu@users.noreply.github.com>
2024-11-17 07:34:34 +0100

    added logic to upload images to the collection with upload_images.py

---
+++ upload_images.py
@@ -0,0 +1,36 @@
+import os
+import uuid
+import chromadb
+from chromadb import PersistentClient
+from chromadb.utils.data_loaders import ImageLoader
+from chromadb.utils.embedding_functions import OpenCLIPEmbeddingFunction                                                    
+
+
+chroma_client = chromadb.PersistentClient(path="my_vectordb")
+
+embedding_function = OpenCLIPEmbeddingFunction()
+data_loader = ImageLoader()
+
+collection = chroma_client.get_collection(name="multimodal_collection", 
+                                          embedding_function=embedding_function,
+                                          data_loader=data_loader)
+
+# Directory containing the images
+image_directory = '1950'
+
+# List all image files in the directory
+image_files = [f for f in os.listdir(image_directory) if f.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp', '.tiff', '.webp'))]
+
+# Generate unique IDs for each image
+ids = [str(uuid.uuid4()) for _ in image_files]
+
+# Create URIs for each image
+uris = [os.path.join(image_directory, f) for f in image_files]
+
+# Add images to the collection
+collection.add(
+    ids=ids,
+    uris=uris
+)
+
+print(collection.count())
